46ade0b7d7f25b8314ac653a310969fe3e726dc2
hari <harikarumuri1086@gmail.com>
2022-11-19 03:35:46 +0530

    Main

---
+++ src/components/home/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Connect</h1>
+            <h2>Create</h2>
+            <h3>conquer</h3>
+          </div>
+          <div className="col"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default home
