0c27bef36388ef39ff29313578476647cf20a989
Toxic-Whitelgnd <whitelegend56@gmail.com>
2022-11-24 23:36:45 +0530

    removed gallery design

--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -8,28 +8,7 @@ export default function Gallery() {
     <div>Gallery
         <div className='gallery-container'>
           
-                <CubeImageAnimation />
-                <CubeImage2Animation />
-                <CubeImageAnimation />
-                <CubeImage2Animation />
-            
-        
-            {/* <div className='nope'>
-            <CubeImageAnimation />
-            </div>
-            
-            <div className='lets'>
-            <CubeImageAnimation />
-            </div>
-            <div className='yo'>
-            <CubeImageAnimation />
-            </div>
-            <div className='mf'>
-            <CubeImageAnimation />
-            </div>
-            <div className='rock'>
-            <CubeImageAnimation />
-            </div> */}
+                <p>Need to add some content here</p>
             
         </div>
         
