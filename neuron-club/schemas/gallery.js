81e2ea4fd50e65f82e542e073ebd25f188cf1056
Toxic-Whitelgnd <whitelegend56@gmail.com>
2022-12-02 12:22:29 +0530

    added gallery by tarun

--- neuron-club/schemas/gallery.js
+++ neuron-club/schemas/gallery.js
@@ -2,18 +2,23 @@ export default {
     title: 'Gallery',
     name: 'gallery',
     type: 'document',
+
+    
+    
+       
     
     fields:[
+        {
+            name: 'imagesGallery',
+            title: 'Images gallery',
+            type: 'array',
+            of: [{ type: 'image' }],
+              
+        },
         {
             name:'gname',
             title:'Name',
             type:'string',
-        },{
-        name:'image',
-        title:'Image',
-        type:'image',
-        Options:{
-            hotspot: true
-        },
-    }]
+        }
+    ]
   }
--- src/components/Gallery/gallery.js
+++ src/components/Gallery/gallery.js
@@ -1,14 +1,45 @@
-import React from 'react'
-import CubeImageAnimation from '../CubeAnimation/cubeimage'
-import CubeImage2Animation from '../CubeAnimation/cubeimageangle'
+import React,{useState,useEffect} from 'react'
 import "../Gallery/gallery.css"
+import {Client} from "../../lib/client"
 
 export default function Gallery() {
+  const [galeryimg,setgalleryimg] = useState(null);
+  useEffect(()=>{
+    Client.fetch(`*[_type == "gallery"]{
+      gname,
+      imagesGallery[]{
+        asset ->{
+          url
+        }
+      }
+    } |order(_updatedAt asc)`).then((data)=> {
+      setgalleryimg(data)
+      console.log(data)
+    })
+    .catch(console.error)
+  },[]);
   return (
     <div>Gallery
         <div className='gallery-container'>
-          
-                <p>Need to add some content here</p>
+          {
+            galeryimg && galeryimg.map((gimg,idx)=>(
+              <div>
+                   <div class="gallery" key={idx}> 
+                   {/* there is a contraint that only 6 images can be added more than can't it wont show less tha u can */}
+                    <img src={gimg.imagesGallery[0].asset.url} alt="yoyo" />
+                    <img src={gimg.imagesGallery[1].asset.url} alt="yoyo" />
+                    <img src={gimg.imagesGallery[2].asset.url} alt="yoyo" />
+                    <img src={gimg.imagesGallery[3].asset.url} alt="yoyo" />
+                    <img src={gimg.imagesGallery[4].asset.url} alt="yoyo" />
+                    <img src={gimg.imagesGallery[5].asset.url} alt="yoyo" />
+                  
+                  </div>
+                  <div className='galleryname mt-4'>
+                    <h4>{gimg.gname}</h4>
+                  </div>
+              </div>
+            ))
+          }
             
         </div>
         
